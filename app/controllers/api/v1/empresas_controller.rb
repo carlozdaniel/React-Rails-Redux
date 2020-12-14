b504bc75bdeec1b57c0ae9d6d46177ed666b1a41
carlos daniel hernandez rodriguez <a20160383@gmail.com>
2020-12-14 02:10:03 -0600

    empresa controller

--- app/controllers/api/v1/empresas_controller.rb
+++ app/controllers/api/v1/empresas_controller.rb
@@ -7,5 +7,24 @@ class Api::V1::EmpresasController < ApplicationController
     @empresas = Empresa.find(params[:id])
     render json: @empresas, include: [:politica]
   end
-  
+  def create
+    @empresas = Empresas.new(params.require(:empresas).permit(:name, :description))
+    if @empresas.save()
+      render json: @empresas, stautus: :created
+    else
+      render json: @empresas.errors, status: :unprossable_entity
+    end
+  def update
+    @empresas Empresa.fine(params[:id])
+    if @empresas.update((params.require(:empresas).permit(:name, :description))
+      render json: @empresas, stautus: :created
+    else
+      render json: @empresas.errors#, status: :unprossable_entity
+    end
+  end
+
+  def destroy
+    @empresas Empresa.fine(params[:id])
+    @empresas.destroy
+  end
 end
